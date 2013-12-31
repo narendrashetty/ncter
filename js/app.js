59779728e0a0adf1cd5e2dba178fce5d8c7c4a8b
Swaroop SM <swaroop.striker@gmail.com>
2013-12-31 09:58:34 +0530

    initial links with animate scroll for nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+
+	$("a[href^='#']").on("click", function(){
+		$('html,body').animate({ scrollTop: $(this.hash).offset().top}, 500);
+		return false;
+	})
+
+});
