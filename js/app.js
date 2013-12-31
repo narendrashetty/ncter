b0e189f74059056d938ec1da4153f6124e33259d
Swaroop SM <swaroop.striker@gmail.com>
2013-12-31 11:39:46 +0530

    added timers

--- js/app.js
+++ js/app.js
@@ -4,9 +4,21 @@ $(document).foundation();
 
 $(document).ready(function(){
 
+	var today = new Date();
+	var event_date = new Date(2014, 2, 22);
+	var submission_date = new Date(2014, 1, 10);
+
+	$("span#event-timer").html(timer_diff(event_date, today) + " DAYS TO GO");
+	$("span#submission-timer").html(timer_diff(submission_date, today) + " DAYS TO GO");
+
 	$("a[href^='#']").on("click", function(){
 		$('html,body').animate({ scrollTop: $(this.hash).offset().top}, 500);
 		return false;
 	})
 
 });
+
+var timer_diff = function(d1, d2){
+	diff = Math.abs(d1 - d2) / 86400000
+	return Math.round(diff)
+};
